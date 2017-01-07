07d26e606595c674933e96901e39111f78d7269c
neil patel <neil@myindianweddingapp.com>
2017-01-07 21:27:21 +0000

    Added a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Starting to run seeds file...."
+
+100.times do |topic|
+  Topic.create!(title: "My Title #{topic}")
+end
+
+puts "100 Topic created"
+
+User.create!(
+  email: "admin@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  first_name: "Jon",
+  last_name: "Snow",
+  username: "wallwatcher",
+  role: "admin"
+)
+
+puts "Admin user created"
+
+User.create!(
+  email: "student@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  first_name: "Jon",
+  last_name: "Snow",
+  username: "youngwallwatcher",
+  role: "student"
+)
+
+puts "Student user created"
